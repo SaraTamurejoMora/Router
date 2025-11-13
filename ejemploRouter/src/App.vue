141564e68c289f787ab798e10de5caaf777f2c01
Sara <sara.tamurejo.7e8@itb.cat>
2025-11-13 16:44:58 +0000

    funciona

---
+++ ejemploRouter/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+<nav>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/frameworks">Frameworks</RouterLink>
+</nav>
+<RouterView></RouterView>
+</template>
+
+<style scoped></style>
---
+++ ejemploRouter/src/views/FrameworkDetail.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+
+const frameworks = ref([
+{ id: 1, name: 'Vue.js', desc: 'Progressive Framework per construir interfícies web.' },
+{ id: 2, name: 'React', desc: 'Biblioteca per crear interfícies d’usuari interactives.' },
+{ id: 3, name: 'Angular', desc: 'Framework complet per aplicacions empresarials.' }
+])
+
+const route = useRoute();
+const frameworkId = parseInt(route.params.id);
+const framework = frameworks.value.find(f => f.id == frameworkId);
+</script>
+
+<template>
+    <h1>{{framework.name}}</h1>
+    <p>{{framework.desc }}</p>
+
+    
+</template>
